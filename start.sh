669d5e51ed0dd1b8ebf968fbfaf0004d85659a05
n3waccounttt <118775206+n3waccounttt@users.noreply.github.com>
2022-12-16 03:35:58 +0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/n3waccounttt/MissPerfect-URL-BOT /MissPerfectURLBOT
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
